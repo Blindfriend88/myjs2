0b1d5f14f2b3d8a8497b64cf762ec5435a04a597
Arnaud <arnaud.delannoy752@gmail.com>
2022-05-24 09:51:03 +0200

    Ajout de la fonction DisplayName

--- ex1.js
+++ ex1.js
@@ -1,3 +1,10 @@
 let _firstName=prompt("Quel est votre prénom?")
 let _surname=prompt("Quel est votre nom de famille?")
-alert("Votre prénom est "+_firstName"+" et votre nom est "+"_surname+".")
+alert("Votre prénom est "+_firstName+" et votre nom est "+_surname+".")
+
+function DisplayName()
+{
+    let _firstName=prompt("Quel est votre prénom?");
+let _surname=prompt("Quel est votre nom de famille?");
+}
+DisplayName(alert("Votre prénom est "+_firstName+" et votre nom est "+_surname+"."))
