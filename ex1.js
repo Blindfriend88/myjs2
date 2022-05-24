0ac049f5379623e22440e3e7e8b0efd272efea00
Arnaud <arnaud.delannoy752@gmail.com>
2022-05-24 10:49:06 +0200

    Ajout de la fonction DisplayFullName

--- ex1.js
+++ ex1.js
@@ -16,3 +16,20 @@ let _surname=prompt("Quel est votre nom de famille?");
 }
 DisplayFirstName(alert("Votre prénom est "+_firstName+"."))
 
+function DisplayFullName()
+{
+    let _firstName=prompt("Quel est votre prénom?");
+let _surname=prompt("Quel est votre nom de famille?");
+if(_firstName=="Arnaud")
+{
+    if(_surname=="Delannoy")
+    {
+        alert("Bonjour majesté!")
+    }
+}
+else
+{
+    alert("Vous vous appelez "+_firstName+" "+_surname+".")
+}
+}
+DisplayFullName()
