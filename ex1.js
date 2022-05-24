138bde1e0bafdcc4ede7328190c1bdae06d964df
Arnaud <arnaud.delannoy752@gmail.com>
2022-05-24 10:09:40 +0200

    Ajout de la fonction DisplayFirstName dans ex1.js puis du fichier index.html dans le répertoire

--- ex1.js
+++ ex1.js
@@ -7,4 +7,12 @@ function DisplayName()
     let _firstName=prompt("Quel est votre prénom?");
 let _surname=prompt("Quel est votre nom de famille?");
 }
-DisplayName(alert("Votre prénom est "+_firstName+" et votre nom est "+_surname+"."))
+DisplayName(alert("Votre nom est "+_surname+"."))
+
+function DisplayFirstName()
+{
+    let _firstName=prompt("Quel est votre prénom?");
+let _surname=prompt("Quel est votre nom de famille?");
+}
+DisplayFirstName(alert("Votre prénom est "+_firstName+"."))
+
